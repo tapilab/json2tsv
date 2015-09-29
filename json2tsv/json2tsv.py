123adf24152f9ccb695ec0703683be3841818cbb
halfak <aaron.halfaker@gmail.com>
2015-09-29 09:08:47 -0500

    Adds py3k support and a --headers arguement to include the headers in the output.

--- json2tsv/json2tsv.py
+++ json2tsv/json2tsv.py
@@ -5,10 +5,19 @@ Convert json to tsv from stdin by extracting the specified objects.
 e.g., cat my.json | json2tsv id user.name
 """
 import argparse
-import codecs
 import json
 import re
 import sys
+
+if bytes == str:  # Python 2.7
+    import codecs
+    open_read_utf8 = lambda f: codecs.getreader('utf8')(f, 'replace')
+    open_write_utf8 = lambda f: codecs.getwriter('utf8')(f, 'replace')
+else:
+    import io
+    open_read_utf8 = lambda f: io.TextIOWrapper(f.buffer, 'utf8', 'replace')
+    open_write_utf8 = lambda f: io.TextIOWrapper(f.buffer, 'utf8', 'replace')
+    unicode = str
 
 
 def rm_ws(s):
@@ -17,12 +26,17 @@ def rm_ws(s):
 
 
 def main():
-    sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+    output = open_write_utf8(sys.stdout)
     ap = argparse.ArgumentParser()
     ap.add_argument('fields', nargs='+', help='fields to print')
+    ap.add_argument('--headers', action="store_true", help='fields to print')
     args = ap.parse_args()
 
-    for line in sys.stdin:
+    if args.headers:
+        output.write('\t'.join(rm_ws(f) for f in args.fields))
+        output.write('\n')
+
+    for line in open_read_utf8(sys.stdin):
         try:
             j = json.loads(line)
             if type(j) is dict:  # as opposed to a list of dicts.
@@ -34,9 +48,9 @@ def main():
                         obj = jj
                         for p in parts:
                             obj = obj[p]
-                        sys.stdout.write('%s\t' % rm_ws(obj))
+                        output.write('%s\t' % rm_ws(obj))
                     else:
-                        sys.stdout.write('%s\t' % rm_ws(jj[field]))
-                sys.stdout.write('\n')
+                        output.write('%s\t' % rm_ws(jj[field]))
+                output.write('\n')
         except Exception as e:
             sys.stderr.write('bad line %s\n' % e)
