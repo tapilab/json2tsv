41bccfea38b7e031c966fe31b2300f6a1cbcfaa2
halfak <aaron.halfaker@gmail.com>
2019-12-19 14:14:56 -0600

    Implements basic tests.

--- tests/test_json2tsv.py
+++ tests/test_json2tsv.py
@@ -7,22 +7,30 @@ test_json2tsv
 
 Tests for `json2tsv` module.
 """
-
-import unittest
+import io
+import json
 
 from json2tsv import json2tsv
 
 
-class TestJson2tsv(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def test_something(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
+def test_basic_call():
+    input = io.StringIO(
+        json.dumps({'foo': 1, 'bar': {'baz': "stuff"}}) + "\n" +
+        json.dumps({'foo': 2, 'bar': {'baz': None}}) + "\n" +
+        json.dumps({'foo': 3, 'bar': {'baz': "stuff"}}) + "\n" +
+        json.dumps({'foo': 4}) + "\n" +
+        json.dumps({'foo': 5, 'bar': [1, 2, 3]}) + "\n" +
+        json.dumps({'foo': 6, 'bar': {'baz': "stuff"}}) + "\n")
+    output = io.StringIO()
+    fields = ["foo", "bar.baz"]
+    headers = True
+    json2tsv.run(input, output, fields, headers)
+    output.seek(0)
+    assert output.read() == \
+        "foo\tbar.baz\n" + \
+        "1\tstuff\n" + \
+        "2\tnull\n" + \
+        "3\tstuff\n" + \
+        "4\tnull\n" + \
+        "5\tnull\n" + \
+        "6\tstuff\n"
